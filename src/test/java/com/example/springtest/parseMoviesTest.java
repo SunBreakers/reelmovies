4881449e88ec5e7dfa68bc03b245191860d83593
julianrowe <julianrowe123@gmail.com>
2022-10-15 19:45:11 -0700

    created JUnit test for parseMovies

---
+++ src/test/java/com/example/springtest/parseMoviesTest.java
@@ -0,0 +1,34 @@
+package com.example.springtest;
+
+import org.junit.*;
+
+public class parseMoviesTest {
+    private parseMovies m;
+
+    @Before
+    public void setup() {
+        System.out.println("before testing ...");
+        m = new parseMovies();
+    }
+
+    @After
+    public void cleanup() {
+        System.out.println("Finishing and cleaning up the test ...");
+    }
+
+    // Julian Rowe
+    @Test
+    public void testParseMovieTitle() {
+        m.setMovie(550);
+        String testTitle = m.getTitle();
+        Assert.assertEquals("Fight Club", testTitle);
+    }
+
+    @Test
+    public void testParseMovieOverview() {
+        parseMovies m = new parseMovies();
+        m.setMovie(550);
+        String testOverview = m.getOverview();
+        Assert.assertEquals("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.", testOverview);
+    }
+}

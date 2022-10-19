3114339f23f9a31d2083f8f0a9a1ca2345b11c9e
norivera <89235780+norivera@users.noreply.github.com>
2022-10-18 18:58:39 -0700

    Create a test for movie run time
    Create a test for movie run time

--- src/test/java/com/example/springtest/parseMoviesTest.java
+++ src/test/java/com/example/springtest/parseMoviesTest.java
@@ -31,4 +31,12 @@ public class parseMoviesTest {
         String testOverview = m.getOverview();
         Assert.assertEquals("A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.", testOverview);
     }
+
+    //Noe Rivera
+    @Test
+    public void testGetRunTime(){
+        m.setMovie(550);
+        int testRunTime = 139;
+        Assert.assertEquals(139, testRunTime);
+    }
 }
